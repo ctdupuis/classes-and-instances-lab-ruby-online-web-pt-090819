3ac4997bf55b0d8dc66583d7dfd02c3eca5ef5ba
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-19 21:46:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog 
+  "Man's best friend"
+end
+
+fido = Dog.new 
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  "Dangerous life-form"
+end
+
+adele_goldberg = Person.new 
+alan_kay = Person.new
